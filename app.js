b80989a85f7795ac3f66a370bfb39f1dcc1f3f7e
Zane Schaffer <contact@zane.wiki>
2021-11-28 00:07:29 -0800

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+const sqlite3 = require('sqlite3').verbose();
+
+let app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+let db = new sqlite3.Database('./iChing.sqlite', (err) => {
+    if (err) {
+        console.error(err)
+    }
+    console.log('Connected to the iChing database.')
+})
+
+app.post('/', function(req, res) {
+    db.all("SELECT * FROM hexagrams WHERE binary =" + req.body.binary, function(err, rows) {
+        rows.forEach(function (row) {
+            console.log(row);
+        })
+    });
+    db.close();
+})
+
+module.exports = app;
