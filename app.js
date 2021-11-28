d15e8dbf99021188b4ad7fdd0de20d654005d68e
Zane Schaffer <contact@zane.wiki>
2021-11-28 00:39:38 -0800

    actually respond with something

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let express = require('express');
-let cors = require('cors');
 
 const sqlite3 = require('sqlite3').verbose();
 
 let app = express();
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 let db = new sqlite3.Database('./iChing.sqlite', (err) => {
